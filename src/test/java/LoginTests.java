df6902a8f02c6d0349544a7e48e470d70973e864
czar-testpro <czar@testpro.io>
2023-05-09 01:23:43 +0800

    Deleting previous unrelated homeworks

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -2,8 +2,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import pom.HomePage;
-import pom.LoginPage;
+import pagefactory.HomePage;
+import pagefactory.LoginPage;
 
 
 public class LoginTests extends BaseTest {
@@ -43,19 +43,30 @@ public class LoginTests extends BaseTest {
         Assert.assertTrue(avatarIcon.isDisplayed());
     }
 
-    //Page Object Model example
+
+    //Fluent interfaces example
     @Test
     public void LoginValidEmailPasswordTest () {
 
         LoginPage loginPage = new LoginPage(driver);
         HomePage homePage = new HomePage(driver);
 
-        loginPage.provideEmail("demo@class.com");
-        loginPage.providePassword("te$t$tudent");
-        loginPage.clickSubmit();
+        loginPage.provideEmail("demo@class.com")
+                 .providePassword("te$t$tudent")
+                 .clickSubmit();
 
-        Assert.assertTrue(homePage.getUserAvatar().isDisplayed());
+        Assert.assertTrue(homePage.isAvatarDisplayed());
+    }
 
+    //    OR
+    @Test
+    public void LoginEmptyEmailPasswordTest() {
+
+        LoginPage loginPage = new LoginPage(driver);
+
+        loginPage.provideEmail("").providePassword("te$t$tudent").clickSubmit();
+
+        Assert.assertEquals(driver.getCurrentUrl(), url);
     }
 
 }
