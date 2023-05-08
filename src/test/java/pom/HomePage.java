392579cdb62650ceb43d3b26ac1a4f7a54243310
czar-testpro <czar@testpro.io>
2023-05-09 04:23:41 +0800

    Update HomePage
    Removing of unused methods

--- src/test/java/pom/HomePage.java
+++ src/test/java/pom/HomePage.java
@@ -1,7 +1,5 @@
 package pom;
-
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
@@ -9,23 +7,8 @@ public class HomePage extends BasePage {
     public HomePage(WebDriver givenDriver) {
         super(givenDriver);
     }
-    By firstPlaylist = By.cssSelector(".playlist:nth-child(3)");
-    By playlistNameField = By.cssSelector("[name='name']");
     By userAvatarIcon = By.cssSelector("img.avatar");
     public WebElement getUserAvatar () {
         return findElement(userAvatarIcon);
-    }
-    public void doubleClickPlaylist() {
-        doubleClick(firstPlaylist);
-    }
-    public void enterNewPlaylistName(String playlistName) {
-        findElement(playlistNameField).sendKeys(playlistName);
-        findElement(playlistNameField).sendKeys(Keys.chord(Keys.CONTROL,"a", Keys.BACK_SPACE));
-        findElement(playlistNameField).sendKeys(playlistName);
-        findElement(playlistNameField).sendKeys(Keys.ENTER);
-    }
-    public boolean doesPlaylistExist(String playlistName) {
-        By newPlaylist = By.xpath("//a[text()='"+playlistName+"']");
-        return findElement(newPlaylist).isDisplayed();
     }
 }
