e1504c8f9f0ecb4fafb0a1cac56c63b99dfeaf50
czar-testpro <czar@testpro.io>
2023-06-29 09:40:06 +0800

    Update LoginTests

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -17,7 +17,7 @@ public class LoginTests extends BaseTest {
         providePassword("te$t$tudent");
         clickSubmit();
 
-        Assert.assertEquals(driver.getCurrentUrl(), url); //https://bbb.testpro.io/
+        Assert.assertEquals(driver.getCurrentUrl(), url); //https://qa.koel.app/
     }
 
     @Test (enabled = true, priority = 1, description = "Login with valid email and valid password")
@@ -38,7 +38,7 @@ public class LoginTests extends BaseTest {
         providePassword("");
         clickSubmit();
 
-        Assert.assertEquals(driver.getCurrentUrl(), url); //https://bbb.testpro.io/
+        Assert.assertEquals(driver.getCurrentUrl(), url); //https://qa.koel.app/
     }
     public static void isAvatarDisplayed() {
         WebElement avatarIcon = driver.findElement(By.cssSelector("img[class='avatar']"));
