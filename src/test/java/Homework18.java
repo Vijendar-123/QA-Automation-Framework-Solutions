d994c0948e85105c5a5f20dedd61ebd2454a9533
czar-testpro <czar@testpro.io>
2023-07-10 22:16:20 +0800

    Update Homework18.java

--- src/test/java/Homework18.java
+++ src/test/java/Homework18.java
@@ -5,7 +5,7 @@ import org.testng.annotations.Test;
 
 public class Homework18 extends BaseTest {
     @Test
-    public void playASongTest() {
+    public void playSong() {
         provideEmail("demo@class.com");
         providePassword("te$t$tudent");
         clickSubmit();
