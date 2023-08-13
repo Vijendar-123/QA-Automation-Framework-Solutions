f8a6619f104186222c35087f445168c949fc722c
czar-testpro <czar@testpro.io>
2023-08-14 02:43:26 +0800

    Update LoginTests

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -10,9 +10,42 @@ import java.time.Duration;
 public class LoginTests extends BaseTest {
     static ChromeOptions options = new ChromeOptions();
     @Test
+    public void loginValidEmailPassword(){
+        // Pre-condition
+        // Added ChromeOptions argument below to fix websocket error
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        // Steps
+        String url = "https://qa.koel.app/";
+        driver.get(url);
+
+        WebElement emailField = driver.findElement(By.cssSelector("input[type='email']"));
+        emailField.clear();
+        emailField.sendKeys("demo@class.com");
+
+        WebElement passwordField = driver.findElement(By.cssSelector("input[type='password']"));
+        passwordField.clear();
+        passwordField.sendKeys("te$t$tudent");
+
+        WebElement submit = driver.findElement(By.cssSelector("button[type='submit']"));
+        submit.click();
+
+        WebElement avatarIcon = driver.findElement(By.cssSelector("img[class='avatar']"));
+        // Expected Result
+        Assert.assertTrue(avatarIcon.isDisplayed());
+
+        driver.quit();
+    }
+
+    @Test
     public void loginInvalidEmailValidPassword(){
         // Pre-condition
         // Added ChromeOptions argument below to fix websocket error
+        ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
 
         WebDriver driver = new ChromeDriver(options);
@@ -27,7 +60,6 @@ public class LoginTests extends BaseTest {
         emailField.sendKeys("invalid@class.com");
 
         WebElement passwordField = driver.findElement(By.cssSelector("input[type='password']"));
-        passwordField.click();//not needed
         passwordField.clear();
         passwordField.sendKeys("te$t$tudent");
 
@@ -42,43 +74,16 @@ public class LoginTests extends BaseTest {
     }
 
     @Test
-    public void loginValidEmailPassword(){
-//      Added ChromeOptions argument below to fix websocket error
-        options.addArguments("--remote-allow-origins=*");
-
-        WebDriver driver = new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-
-        String url = "https://qa.koel.app/";
-        driver.get(url);
-
-        WebElement emailField = driver.findElement(By.cssSelector("input[type='email']"));
-        emailField.clear();
-        emailField.sendKeys("demo@class.com");
-
-        WebElement passwordField = driver.findElement(By.cssSelector("input[type='password']"));
-        passwordField.click();//not needed
-        passwordField.clear();
-        passwordField.sendKeys("te$t$tudent");
-
-        WebElement submit = driver.findElement(By.cssSelector("button[type='submit']"));
-        submit.click();
-
-        WebElement avatarIcon = driver.findElement(By.cssSelector("img[class='avatar']"));
-
-        Assert.assertTrue(avatarIcon.isDisplayed());
-
-        driver.quit();
-    }
-
-    @Test
     public void loginValidEmailEmptyPassword() {
-//      Added ChromeOptions argument below to fix websocket error
+        // Pre-condition
+        // Added ChromeOptions argument below to fix websocket error
+        ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
 
         WebDriver driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
+        // Steps
         String url = "https://qa.koel.app/";
         driver.get(url);
 
@@ -86,15 +91,13 @@ public class LoginTests extends BaseTest {
         emailField.clear();
         emailField.sendKeys("demo@class.com");
 
-        WebElement passwordField = driver.findElement(By.cssSelector("input[type='password']"));
-        passwordField.clear();
-        passwordField.sendKeys("");
-
         WebElement submit = driver.findElement(By.cssSelector("button[type='submit']"));
         submit.click();
 
+        // Expected Result
         Assert.assertEquals(driver.getCurrentUrl(), url); //https://qa.koel.app/
 
+        // Post-condition
         driver.quit();
     }
 }
