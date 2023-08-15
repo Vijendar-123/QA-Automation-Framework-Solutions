b0b4d4d2b08b9c28e3b391fa1aba95e5f5ca9b1c
czar-testpro <czar@testpro.io>
2023-08-15 13:55:17 +0800

    Updated Homework15.java

--- src/test/java/Homework15.java
+++ src/test/java/Homework15.java
@@ -1,6 +1,5 @@
-import org.openqa.selenium.By;
+
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
@@ -10,21 +9,21 @@ import java.time.Duration;
 
 public class Homework15 extends BaseTest {
 
-    static ChromeOptions options = new ChromeOptions();
+
     @Test
     public void LoginEmptyEmailPassword() throws InterruptedException {
-        // Simple comment
 
-//      Added ChromeOptions argument below to fix websocket error
+        // Added ChromeOptions argument below to fix websocket error
+        ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
+
         WebDriver driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
         String url = "https://testpro.io/";
-        Thread.sleep(2000);
         driver.get(url);
+
         Assert.assertEquals(driver.getCurrentUrl(), url);
-        Thread.sleep(2000);
         driver.quit();
 
     }
