186a20f89a1a3d6ebf25457238edd6ea1b7e632b
czar-testpro <czar@testpro.io>
2023-09-04 15:32:13 +0800

    Updated HomePage

--- src/test/java/pagefactory/HomePage.java
+++ src/test/java/pagefactory/HomePage.java
@@ -6,12 +6,19 @@ import org.openqa.selenium.support.FindBy;
 public class HomePage extends BasePage {
     @FindBy(css = "a.view-profile")
     WebElement profileIcon;
+
+    @FindBy(css = "img[class='avatar']")
+    WebElement avatarIcon;
     public HomePage(WebDriver givenDriver) {
         super(givenDriver);
     }
     public HomePage clickProfileIcon(){
         click(profileIcon);
         return this;
+    }
+
+    public boolean isAvatarDisplayed(){
+        return avatarIcon.isDisplayed();
     }
 }
 
