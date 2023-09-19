5ee2a13923cd50e09629e1610c56ad1fcdfd1c0f
czar-testpro <czar@testpro.io>
2023-09-19 20:42:33 +0800

    Updated LoginTests

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,5 +1,3 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import pom.HomePage;
@@ -8,44 +6,10 @@ import pom.LoginPage;
 
 public class LoginTests extends BaseTest {
 
-    @Test (dataProvider = "IncorrectLoginData", dataProviderClass = BaseTest.class, enabled = true, priority = 0, description = "Login with invalid email and valid password")
-    public void loginInvalidEmailValidPassword(String username, String password){
-
-        provideEmail(username);
-        providePassword(password);
-        clickSubmit();
-
-        Assert.assertEquals(driver.getCurrentUrl(), url); //https://qa.koel.app/
-    }
-
-    @Test (enabled = true, priority = 1, description = "Login with valid email and valid password")
-    public void loginValidEmailPassword(){
-
-        navigateToPage();
-        provideEmail("demo@class.com");
-        providePassword("te$t$tudent");
-        clickSubmit();
-        isAvatarDisplayed();
-    }
-
-    @Test (enabled = true, priority = 3, description = "Login with valid email and empty password")
-    public void loginValidEmailEmptyPassword() {
-
-        navigateToPage();
-        provideEmail("demo@class.com");
-        providePassword("");
-        clickSubmit();
-
-        Assert.assertEquals(driver.getCurrentUrl(), url); //https://qa.koel.app/
-    }
-    public static void isAvatarDisplayed() {
-        WebElement avatarIcon = driver.findElement(By.cssSelector("img[class='avatar']"));
-        Assert.assertTrue(avatarIcon.isDisplayed());
-    }
 
     //Page Object Model example
     @Test
-    public void LoginValidEmailPassword() {
+    public void loginValidEmailPassword() {
 
         LoginPage loginPage = new LoginPage(driver);
         HomePage homePage = new HomePage(driver);
