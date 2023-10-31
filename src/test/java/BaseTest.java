9df3abf42314182c999a783fea711a511302105c
czar-testpro <czar@testpro.io>
2023-10-31 18:51:43 +0800

    Updated BaseTest
    Removed extra codes not needed for Cucumber lesson

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -23,112 +23,11 @@ import java.util.UUID;
 
 public class BaseTest {
 
-    public  WebDriver driver = null;
-    public  String url = null;
-    public ThreadLocal<WebDriver> threadDriver = null;
-
     @BeforeSuite
     static void setupClass() {
-//        WebDriverManager.chromedriver().setup();
+        WebDriverManager.chromedriver().setup();
     }
 
-    @BeforeMethod
-    @Parameters({"BaseURL"})
-    public void launchBrowser(String BaseURL) throws MalformedURLException {
-        threadDriver.set(pickBrowser(System.getProperty("browser")));
-        getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
-        url = BaseURL;
-        navigateToPage();
-    }
-
-    public WebDriver getDriver() {
-        return threadDriver.get();
-    }
-    @AfterMethod
-    public void closeBrowser() {
-        getDriver().quit();
-        threadDriver.remove();
-    }
-
-    public WebDriver lambdaTest() throws MalformedURLException {
-
-/*      Test Pro Instructor LambdaTest account
-
-        1.) Navigate to https://accounts.lambdatest.com/login
-
-        2.) Login using Google email
-
-        Email: lambdatest.testpro@gmail.com
-        Password: testpro123
-
-        3.) Run command in IntelliJ Terminal:
-         gradle clean test -Dbrowser=cloud
-
-        4.) View the cloud automations in
-        https://accounts.lambdatest.com/dashboard
-
-
-       Configured for the Test Pro lambdatest account
-  */
-        String hubURL = "https://hub.lambdatest.com/wd/hub";
-
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("browserName", "Firefox");
-        capabilities.setCapability("browserVersion", "107.0");
-        HashMap<String, Object> ltOptions = new HashMap<>();
-        ltOptions.put("user", "lambdatest.testpro");
-        ltOptions.put("accessKey", "Op3WvHgSXBtuyR1TVO1wnBgA6qG34RvRcL9HWa8HLKzX4kSf5B");
-        ltOptions.put("build", "Selenium 4");
-        ltOptions.put("name", this.getClass().getName());
-        ltOptions.put("platformName", "Windows 10");
-        ltOptions.put("seCdp", true);
-        ltOptions.put("selenium_version", "4.0.0");
-        capabilities.setCapability("LT:Options", ltOptions);
-
-        return new RemoteWebDriver(new URL(hubURL), capabilities);
-    }
-
-    public WebDriver pickBrowser(String browser) throws MalformedURLException {
-        DesiredCapabilities caps = new DesiredCapabilities();
-        String gridURL = "http://192.168.55.103:4444";//replace with your grid url
-
-        switch(browser) {
-            case "firefox": // gradle clean test -Dbrowser=firefox
-                WebDriverManager.firefoxdriver().setup();
-                return driver = new FirefoxDriver();
-
-            case "MicrosoftEdge": // gradle clean test -Dbrowser=MicrosoftEdge
-                WebDriverManager.edgedriver().setup();
-                EdgeOptions edgeOptions = new EdgeOptions();
-                edgeOptions.addArguments("--remote-allow-origins=*");
-                return driver = new EdgeDriver(edgeOptions);
-
-            case "grid-edge": // gradle clean test -Dbrowser=grid-edge
-                caps.setCapability("browserName", "MicrosoftEdge");
-                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(), caps);
-
-            case "grid-firefox": // gradle clean test -Dbrowser=grid-firefox
-                caps.setCapability("browserName", "firefox");
-                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(), caps);
-
-            case "grid-chrome": // gradle clean test -Dbrowser=grid-chrome
-                caps.setCapability("browserName", "chrome");
-                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(), caps);
-
-            case "cloud": // gradle clean test -Dbrowser=cloud
-                return lambdaTest();
-
-            default:
-                WebDriverManager.chromedriver().setup();
-                ChromeOptions chromeOptions = new ChromeOptions();
-                chromeOptions.addArguments("--remote-allow-origins=*");
-                return driver = new ChromeDriver(chromeOptions);
-        }
-    }
-
-    public  void navigateToPage() {
-        getDriver().get(url);
-    }
 
 }
