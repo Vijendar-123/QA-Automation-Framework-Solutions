496ae8af59cd02ed33025e861804597c2e3564c6
czar-testpro <czar@testpro.io>
2024-01-28 01:45:22 +0800

    Updated LoginTests and BaseTest

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -20,21 +20,10 @@ import java.time.Duration;
 import java.util.UUID;
 
 public class BaseTest {
-
     public static WebDriver driver = null;
     public static String url = null;
     public static WebDriverWait wait = null;
     public static Actions actions = null;
-
-    @DataProvider(name="IncorrectLoginData")
-    public Object[][] getDataFromDataProviders() {
-
-        return new Object[][] {
-                {"invalid@mail.com", "invalidPass"},
-                {"demo@class.com", ""},
-                {"", ""}
-        };
-    }
 
     @BeforeSuite
     static void setupClass() {
@@ -62,47 +51,12 @@ public class BaseTest {
     public  void navigateToPage() {
         driver.get(url);
     }
-    public void provideEmail(String email) {
-        WebElement emailField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input[type='email']")));
-        emailField.clear();
-        emailField.sendKeys(email);
-    }
-    public void providePassword(String password) {
-        WebElement passwordField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input[type='password']")));
-        passwordField.clear();
-        passwordField.sendKeys(password);
-    }
-    public void clickSubmit() {
-        WebElement submit = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("button[type='submit']")));
-        submit.click();
-    }
-    public void clickSaveButton() {
-        WebElement saveButton = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("button.btn-submit")));
-        saveButton.click();
-    }
-    public void provideProfileName(String randomName) {
-        WebElement profileName = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[name='name']")));
-        profileName.clear();
-        profileName.sendKeys(randomName);
-    }
-    public void provideCurrentPassword(String password) {
-        WebElement currentPassword = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[name='current_password']")));
-        currentPassword.clear();
-        currentPassword.sendKeys(password);
-    }
-    public String generateRandomName() {
-        return UUID.randomUUID().toString().replace("-", "");
-    }
-    public void clickAvatarIcon() {
-        WebElement avatarIcon = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("img.avatar")));
-        avatarIcon.click();
-    }
 
     public static WebDriver pickBrowser(String browser) throws MalformedURLException {
         DesiredCapabilities caps = new DesiredCapabilities();
         String gridURL = "http://192.168.55.100:4444";//replace with your grid url
 
-        //java -jar selenium-server-4.15.0.jar standalone --selenium-manager true
+        //java -jar selenium-server-4.XX.0.jar standalone --selenium-manager true
 
         switch(browser) {
             case "firefox": // gradle clean test -Dbrowser=firefox
--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,61 +1,33 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import pom.HomePage;
-import pom.LoginPage;
+import pagefactory.HomePage;
+import pagefactory.LoginPage;
 
 
 public class LoginTests extends BaseTest {
 
-    @Test (dataProvider = "IncorrectLoginData", dataProviderClass = BaseTest.class, enabled = true, priority = 0, description = "Login with invalid email and valid password")
-    public void loginInvalidEmailValidPasswordTest(String username, String password){
-
-        provideEmail(username);
-        providePassword(password);
-        clickSubmit();
-
-        Assert.assertEquals(driver.getCurrentUrl(), url); //https://bbb.testpro.io/
-    }
-
-    @Test (enabled = true, priority = 1, description = "Login with valid email and valid password")
-    public void loginValidEmailPasswordTest(){
-
-        navigateToPage();
-        provideEmail("demo@class.com");
-        providePassword("te$t$tudent");
-        clickSubmit();
-        isAvatarDisplayed();
-    }
-
-    @Test (enabled = true, priority = 3, description = "Login with valid email and empty password")
-    public void loginValidEmailEmptyPasswordTest() {
-
-        navigateToPage();
-        provideEmail("demo@class.com");
-        providePassword("");
-        clickSubmit();
-
-        Assert.assertEquals(driver.getCurrentUrl(), url); //https://bbb.testpro.io/
-    }
-    public static void isAvatarDisplayed() {
-        WebElement avatarIcon = driver.findElement(By.cssSelector("img[class='avatar']"));
-        Assert.assertTrue(avatarIcon.isDisplayed());
-    }
-
-    //Page Object Model example
+    //Fluent interfaces example
     @Test
-    public void LoginValidEmailPasswordTest () {
+    public void loginValidEmailPassword () {
 
         LoginPage loginPage = new LoginPage(driver);
         HomePage homePage = new HomePage(driver);
 
-        loginPage.provideEmail("demo@class.com");
-        loginPage.providePassword("te$t$tudent");
-        loginPage.clickSubmit();
+        loginPage.provideEmail("demo@class.com").providePassword("te$t$tudent").clickSubmit();
 
-        Assert.assertTrue(homePage.getUserAvatar().isDisplayed());
+        Assert.assertTrue(homePage.isAvatarDisplayed());
+    }
 
+    //    OR
+    @Test
+    public void loginEmptyEmailPassword() throws InterruptedException {
+
+        LoginPage loginPage = new LoginPage(driver);
+
+        loginPage.provideEmail("").providePassword("te$t$tudent").clickSubmit();
+
+        Thread.sleep(2000);
+        Assert.assertEquals(driver.getCurrentUrl(), url);
     }
 
 }
